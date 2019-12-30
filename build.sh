cd3de271db60b07a4f00df2deeed78353546e86b
toneysix <toneysix@hotmail.com>
2019-12-30 15:19:44 -0500

    Fixed build for *unix

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
+
 if ! type "dotnet" > /dev/null; then
 	echo "Compiler isn't detected, please install dotnet manually before run build.sh"
 fi
 
 dotnet --version
 origPath=$PATH
+binDir="$PWD/bin/"
 
 export DOTNET_HOME="$DOTNET_INSTALL_DIR/cli"
 export PATH="$DOTNET_HOME/bin:$PATH"
@@ -15,16 +17,16 @@ echo "Done restoring."
 
 # Build
 echo "Building library..."
-for d in src/*.csproj; do 
-        dotnet build $d
-    fi
+for d in src/*/*.csproj;
+do
+        dotnet build "$d" -o $binDir -c Release --no-restore
 done
 echo "Done building library."
 
 # Test
-for d in tests/*.csproj; do 
+for d in tests/*/*.csproj; do 
     echo "Testing $d"
-    dotnet test $d
+    dotnet test "$d" -c Release --no-restore
 done
 echo "Build Complete."
 
